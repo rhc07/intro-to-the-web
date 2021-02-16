114e1ed27403e6499dfe484564f2e177c8ceb95c
Rorie Clarke <Rorie@MacBook-Pro.local>
2021-02-16 14:40:03 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+
+configure do
+  set :session_secret, "My session secret"
+end
+
+get '/' do
+  "Hello Rorie"
+end
+
+get '/secret' do
+  "Another message"
+end
+
+get '/other_route' do
+  "how many other routes can we think of?"
+end
+
+get '/hello' do
+  "Hello, is it me you're looking for?"
+end
+
+get '/cat' do
+  erb(:index)
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+  # "<div style= 'border: 3px dashed red'>
+  # <img src = 'https://media.newyorker.com/photos/5dfab39dde5fcf00086aec77/16:9/w_1280,c_limit/Lane-Cats.jpg'>
+  # </div>"
+end
